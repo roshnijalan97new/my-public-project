21db354db1e04c8241f72c63b343bfd38bf43787
Roshni Jalan <roshni.jalan@sap.com>
2024-10-04 21:30:04 +0530

    fixed add function to calculate sum of any no of input

--- main/StringCalculator.js
+++ main/StringCalculator.js
@@ -1,9 +1,9 @@
 function add(numbers) {
-  const num1 = parseInt(numbers.split(",")[0]);
-  const num2 = parseInt(numbers.split(",")[1]);
-  if (num2) return num1 + num2;
-  else if (num1) return num1;
-  else return 0;
+    if (numbers === "") {
+        return 0;
+    }
+    const numArray = numbers.split(",");
+    return numArray.reduce((total, num) => total + parseInt(num), 0);
 }
 
 module.exports = add;

ad610484c6a990fa4773504c1fe434174bbde809
Roshni Jalan <roshni.jalan@sap.com>
2024-10-04 21:43:39 +0530

    fixed add function to handle different delimiters

--- main/StringCalculator.js
+++ main/StringCalculator.js
@@ -1,10 +1,17 @@
 function add(numbers) {
-    if (numbers === "") {
-        return 0;
-    }
-    const delimiters=/[\n,]/; //split the input by comma or new line
-    const numArray = numbers.split(delimiters);
-    return numArray.reduce((total, num) => total + parseInt(num), 0);
+  if (numbers === "") {
+    return 0;
+  }
+
+  let delimiters = /[\n,]/; //it should separate numbers by newline and comma character
+  if (numbers.startsWith("//")) {
+    const delimiterInfo = numbers.split("\n")[0];
+    delimiters = new RegExp(delimiterInfo[2]);
+    numbers = numbers.split("\n")[1];
+  }
+
+  const numArray = numbers.split(delimiters);
+  return numArray.reduce((total, num) => total + parseInt(num), 0);
 }
 
 module.exports = add;

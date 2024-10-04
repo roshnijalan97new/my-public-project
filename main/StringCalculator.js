6b842abbba6cbebac610d3a13b454aa81f1326db
Roshni Jalan <roshni.jalan@sap.com>
2024-10-04 21:07:31 +0530

    adding test cases and dummy function

---
+++ main/StringCalculator.js
@@ -0,0 +1,5 @@
+function add(numbers) { //create a dummy class
+    return numbers;         
+}
+
+module.exports = add;
---
+++ test/StringCalculator.test.js
@@ -0,0 +1,13 @@
+const add = require('../main/StringCalculator');
+
+test('should return 0 for an empty string', () => {
+    expect(add("")).toBe(0);
+});
+
+test('should return the number itself when there is one number', () => {
+    expect(add("1")).toBe(1);
+});
+
+test('should return the sum of two numbers', () => {
+    expect(add("1,2")).toBe(3);
+});

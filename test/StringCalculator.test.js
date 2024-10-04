be40baa5badc084780724149271472d4d8cb4801
Roshni Jalan <roshni.jalan@sap.com>
2024-10-04 21:31:21 +0530

    added test case to handle new line between numbers

--- test/StringCalculator.test.js
+++ test/StringCalculator.test.js
@@ -12,7 +12,11 @@ test('should return the number itself when there is one number', () => {
 test('should return the sum of two numbers', () => {
     expect(add("1,2")).toBe(3);
 });
-//add method to handle any amount of numbers
+//Allow the add method to handle any amount of numbers
 test('should return the sum of multiple numbers', () => {
     expect(add("1,2,3,4")).toBe(10);
+});
+//Allow the add method to handle new lines between numbers (instead of commas). ("1\n2,3" should return 6)
+test('should return the sum when newlines are used as delimiters', () => {
+    expect(add("1\n2,3")).toBe(6);
 });

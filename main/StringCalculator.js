2676776433abed45274a59a605a892fc81c7994a
Roshni Jalan <roshni.jalan@sap.com>
2024-10-04 21:20:13 +0530

    implementing add function according to test cases

--- main/StringCalculator.js
+++ main/StringCalculator.js
@@ -1,5 +1,9 @@
-function add(numbers) { //create a dummy class
-    return numbers;         
+function add(numbers) {
+  const num1 = parseInt(numbers.split(",")[0]);
+  const num2 = parseInt(numbers.split(",")[1]);
+  if (num2) return num1 + num2;
+  else if (num1) return num1;
+  else return 0;
 }
 
 module.exports = add;

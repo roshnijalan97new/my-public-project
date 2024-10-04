79d95eaecf0f6927a0c6c86a548e1dca3b9156d0
Roshni Jalan <roshni.jalan@sap.com>
2024-10-04 21:27:57 +0530

    added test case to handle any no of inputs

--- test/StringCalculator.test.js
+++ test/StringCalculator.test.js
@@ -1,5 +1,6 @@
 const add = require('../main/StringCalculator');
 
+// Simple add function Test Cases
 test('should return 0 for an empty string', () => {
     expect(add("")).toBe(0);
 });
@@ -10,4 +11,8 @@ test('should return the number itself when there is one number', () => {
 
 test('should return the sum of two numbers', () => {
     expect(add("1,2")).toBe(3);
+});
+//add method to handle any amount of numbers
+test('should return the sum of multiple numbers', () => {
+    expect(add("1,2,3,4")).toBe(10);
 });
